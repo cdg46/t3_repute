daa4e284d2c6296df329a69cc056620aaa0f0c78
root <root@46-typo3-dev.ip-5-39-66.eu>
2015-06-24 16:01:53 +0200

    + ext_tables.php pour ajouter les fichiers statics (setup et constants)

---
+++ ext_tables.php
@@ -0,0 +1,12 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die('Access denied.');
+}
+
+
+
+
+/***************
+ * Theme Ville sur Lot TypoScript Setup
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'theme repute');
